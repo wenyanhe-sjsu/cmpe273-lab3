d6dc1d80329c6dce6352d5318dd7eec309a34260
Wenyan He <wenyan.he@sjsu.edu>
2018-09-23 23:49:51 -0700

    Second commit

--- multicast_udp_client.py
+++ multicast_udp_client.py
@@ -7,15 +7,16 @@ from twisted.internet import reactor
 class MulticastPingClient(DatagramProtocol):
 
     def startProtocol(self):
-        #IPaddr = "228.0.0.5"
-        IPaddr = "127.0.0.1"
+        IPaddr = "228.0.0.5"
+        #IPaddr = "127.0.0.1"
         port = 9999
 
         # Join the multicast address, so we can receive replies:
         self.transport.joinGroup(IPaddr)
         # Send to IPaddr:port - all listeners on the multicast address
         # (including us) will receive this message.
-        self.transport.write(b'Client: Ping', (IPaddr, port))
+        # self.transport.write(b'Client: Ping', (IPaddr, port))
+        self.transport.write(b'Hello world', (IPaddr, port))
 
     def datagramReceived(self, datagram, address):
         datagram = datagram.decode()  # remove the 'b' prefix
